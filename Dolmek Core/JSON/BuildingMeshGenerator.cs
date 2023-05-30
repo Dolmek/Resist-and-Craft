054ff184a63d84d7585f49de5eba57ff9270fea5
Dolmek <55515763+Dolmek@users.noreply.github.com>
2023-05-30 21:58:38 +0200

    Update BuildingMeshGenerator.cs

--- Dolmek Core/JSON/BuildingMeshGenerator.cs
+++ Dolmek Core/JSON/BuildingMeshGenerator.cs
@@ -1,22 +1,21 @@
-using UnityEngine;
-using UnityEditor;
-using System.IO;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
 
-public class BuildingMeshGenerator : EditorWindow
+public class BuildingMeshGenerator : UnityEditor.EditorWindow
 {
-    private string jsonFilePath = "Assets/Dolmek Core/JSON/cauldronhope.json";
-    private List<List<Vector2>> buildingCoordinates = new List<List<Vector2>>();
-
-    [MenuItem("Window/Building Mesh Generator")]
+    [UnityEditor.MenuItem("Window/Building Mesh Generator")]
     public static void ShowWindow()
     {
-        GetWindow<BuildingMeshGenerator>("Building Mesh Generator");
+        UnityEditor.EditorWindow.GetWindow(typeof(BuildingMeshGenerator));
     }
+
+    private string jsonFilePath = "Assets/Dolmek Core/JSON/cauldronhope.json";
 
     private void OnGUI()
     {
-        GUILayout.Label("Building Mesh Generator", EditorStyles.boldLabel);
+        GUILayout.Label("Building Mesh Generator", UnityEditor.EditorStyles.boldLabel);
 
         if (GUILayout.Button("Generate Building Mesh"))
         {
@@ -26,81 +25,59 @@ public class BuildingMeshGenerator : EditorWindow
 
     private void GenerateBuildingMesh()
     {
-        string jsonContent = File.ReadAllText(jsonFilePath);
-        JSONObject jsonObject = new JSONObject(jsonContent);
+        string jsonText = System.IO.File.ReadAllText(jsonFilePath);
+        RootObject rootObject = JsonUtility.FromJson<RootObject>(jsonText);
 
-        // Find the "buildings" object in the JSON
-        JSONObject buildingsObject = jsonObject.GetField("buildings");
+        List<Building> buildings = rootObject.buildings;
 
-        // Iterate over the buildings
-        foreach (JSONObject building in buildingsObject.list)
+        foreach (Building building in buildings)
         {
-            string type = building.GetField("type").str;
+            string type = building.type;
+            List<List<List<float>>> coordinates = building.coordinates;
 
-            // Check if the building has a "MultiPolygon" type
-            if (type == "MultiPolygon")
+            List<Vector3> vertices = new List<Vector3>();
+            List<int> triangles = new List<int>();
+
+            foreach (List<List<float>> polygon in coordinates)
             {
-                // Get the coordinates defining the building's outline
-                JSONObject coordinates = building.GetField("coordinates");
-
-                // Iterate over the polygons
-                foreach (JSONObject polygon in coordinates.list)
+                foreach (List<float> coordinate in polygon)
                 {
-                    List<Vector2> buildingContour = new List<Vector2>();
+                    float x = coordinate[0];
+                    float z = coordinate[1];
 
-                    // Iterate over the vertices of the polygon
-                    foreach (JSONObject vertex in polygon.list)
-                    {
-                        float x = vertex.GetField("x").n;
-                        float z = vertex.GetField("z").n;
-                        Vector2 vertexPosition = new Vector2(x, z);
-                        buildingContour.Add(vertexPosition);
-                    }
-
-                    // Add the building's contour to the list of coordinates
-                    buildingCoordinates.Add(buildingContour);
+                    vertices.Add(new Vector3(x, 0f, z));
                 }
             }
+
+            int[] buildingTriangles = Triangulator.Triangulate(vertices);
+            triangles.AddRange(buildingTriangles);
+
+            Mesh mesh = new Mesh();
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangles.ToArray();
+            mesh.RecalculateNormals();
+
+            GameObject buildingObject = new GameObject("Building");
+            MeshFilter meshFilter = buildingObject.AddComponent<MeshFilter>();
+            MeshRenderer meshRenderer = buildingObject.AddComponent<MeshRenderer>();
+
+            meshFilter.sharedMesh = mesh;
+            meshRenderer.material = new Material(Shader.Find("Standard"));
         }
 
-        // Generate the building meshes
-        foreach (List<Vector2> contour in buildingCoordinates)
-        {
-            GenerateMeshFromContour(contour);
-        }
-
-        // Clear the list of building coordinates
-        buildingCoordinates.Clear();
-
-        Debug.Log("Building meshes generated successfully!");
+        Debug.Log("Building mesh generation completed.");
     }
 
-    private void GenerateMeshFromContour(List<Vector2> contour)
+    [System.Serializable]
+    public class Building
     {
-        GameObject buildingObject = new GameObject("Building");
-        MeshFilter meshFilter = buildingObject.AddComponent<MeshFilter>();
-        MeshRenderer meshRenderer = buildingObject.AddComponent<MeshRenderer>();
-        Mesh mesh = new Mesh();
+        public string type;
+        public List<List<List<float>>> coordinates;
+    }
 
-        // Convert the contour vertices to 3D by setting y to 0
-        Vector3[] vertices = new Vector3[contour.Count];
-        for (int i = 0; i < contour.Count; i++)
-        {
-            vertices[i] = new Vector3(contour[i].x, 0f, contour[i].y);
-        }
-
-        // Generate the triangles for the mesh
-        int[] triangles = Triangulator.Triangulate(contour);
-
-        // Assign the vertices and triangles to the mesh
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-
-        // Calculate the mesh normals and bounds
-        mesh.RecalculateNormals();
-        mesh.RecalculateBounds();
-
-        // Assign the mesh to the mesh filter
-        meshFilter.sharedMesh = mesh;
+    [System.Serializable]
+    public class RootObject
+    {
+        public List<Building> buildings;
     }
 }
