6db3a8b83a5309385f3e6e852931dff85b1006cd
Dolmek <55515763+Dolmek@users.noreply.github.com>
2023-05-31 19:16:45 +0200

    Update BuildingMeshGenerator.cs
    Nessun errore nel codice, o nell'Editor ma nessuna mesh generata

--- Dolmek Core/JSON/BuildingMeshGenerator.cs
+++ Dolmek Core/JSON/BuildingMeshGenerator.cs
@@ -27,44 +27,46 @@ public class BuildingMeshGenerator : MonoBehaviour
 
         // Parse JSON data
         MapData mapData = JsonConvert.DeserializeObject<MapData>(jsonFile.text);
-
-        // Create building meshes
-        foreach (Geometry building in mapData.buildings)
+        if (mapData.buildings != null)
         {
-            GameObject buildingObject = new GameObject();
-            buildingObject.transform.SetParent(transform);
-            buildingObject.name = building.type;
-
-            MeshFilter meshFilter = buildingObject.AddComponent<MeshFilter>();
-            MeshRenderer meshRenderer = buildingObject.AddComponent<MeshRenderer>();
-
-            // Create mesh data from building coordinates
-            Mesh mesh = new Mesh();
-            Vector3[] vertices = new Vector3[building.coordinates.Length];
-
-            for (int i = 0; i < building.coordinates.Length; i++)
+            // Create building meshes
+            foreach (Geometry building in mapData.buildings)
             {
-                vertices[i] = new Vector3(building.coordinates[i].x, 0f, building.coordinates[i].y);
+                GameObject buildingObject = new GameObject();
+                buildingObject.transform.SetParent(transform);
+                buildingObject.name = building.type;
+
+                MeshFilter meshFilter = buildingObject.AddComponent<MeshFilter>();
+                MeshRenderer meshRenderer = buildingObject.AddComponent<MeshRenderer>();
+
+                // Create mesh data from building coordinates
+                Mesh mesh = new Mesh();
+                Vector3[] vertices = new Vector3[building.coordinates.Length];
+
+                for (int i = 0; i < building.coordinates.Length; i++)
+                {
+                    vertices[i] = new Vector3(building.coordinates[i].x, 0f, building.coordinates[i].y);
+                }
+
+                int[] triangles = new int[(building.coordinates.Length - 2) * 3];
+                int triangleIndex = 0;
+
+                for (int i = 1; i < building.coordinates.Length - 1; i++)
+                {
+                    triangles[triangleIndex] = 0;
+                    triangles[triangleIndex + 1] = i;
+                    triangles[triangleIndex + 2] = i + 1;
+
+                    triangleIndex += 3;
+                }
+
+                // Assign mesh data to mesh
+                mesh.vertices = vertices;
+                mesh.triangles = triangles;
+
+                meshFilter.mesh = mesh;
+                meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
             }
-
-            int[] triangles = new int[(building.coordinates.Length - 2) * 3];
-            int triangleIndex = 0;
-
-            for (int i = 1; i < building.coordinates.Length - 1; i++)
-            {
-                triangles[triangleIndex] = 0;
-                triangles[triangleIndex + 1] = i;
-                triangles[triangleIndex + 2] = i + 1;
-
-                triangleIndex += 3;
-            }
-
-            // Assign mesh data to mesh
-            mesh.vertices = vertices;
-            mesh.triangles = triangles;
-
-            meshFilter.mesh = mesh;
-            meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
         }
     }
 
